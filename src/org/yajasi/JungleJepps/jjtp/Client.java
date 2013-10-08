c63ba3cc02141c10b657c6269c51ab0acc3af101
joeljeske <joeljeske14@10.39.29.126>
2013-10-08 00:25:01 -0500

    Added implementation of DatabaseConnection

--- src/org/yajasi/JungleJepps/jjtp/Client.java
+++ src/org/yajasi/JungleJepps/jjtp/Client.java
@@ -1,10 +1,29 @@
 package org.yajasi.JungleJepps.jjtp;
 
-public class Client {
+import org.yajasi.JungleJepps.Runway;
+import org.yajasi.JungleJepps.db.DatabaseConnection;
+
+public class Client implements DatabaseConnection{
 	
 
 	public static void main(String[] args){
 	
 	}
+
+	@Override
+	public String[] getAllRunwayIds() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Runway getRunway(String runwayId) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean updateRunway(Runway runway) {
+		throw new UnsupportedOperationException();
+	}
+
 	
 }
