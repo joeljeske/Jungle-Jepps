0f4145dc7618bf4aad1d7176d9731b498dfd0c8f
joeljeske <joeljeske14@10.39.29.126>
2013-10-10 23:29:00 -0500

    Added inner class to support external listening of provider changes

--- src/org/yajasi/JungleJepps/jjtp/JungleJeppsmDNS.java
+++ src/org/yajasi/JungleJepps/jjtp/JungleJeppsmDNS.java
@@ -3,7 +3,9 @@ package org.yajasi.JungleJepps.jjtp;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayDeque;
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
 
 import javax.jmdns.ServiceEvent;
@@ -15,7 +17,8 @@ import org.apache.http.client.utils.URIBuilder;
 
 /**
  * This class manages the client-side and server-side of multicast DNS. 
- * It is capable of broadcasting its services as 
+ * It is capable of broadcasting its services and finding providers 
+ * that match its service critera.
  * @author Joel Jeske
  */
 public class JungleJeppsmDNS {
@@ -28,11 +31,15 @@ public class JungleJeppsmDNS {
 	private static final String MDNS_SERVICE_NAME = "JJTP service";
 	private static final String MDNS_SERVICE_NAME_PRETTY = "Jungle Jepps Desktop Service";
 
+	// Must have 2 different objects for use when discovering and broadcasting
 	private static JmDNS broadcasting, discovery;
 	private static Listener listener;
 	
 	// Client requested server
-	private static Set<URI> providers = new HashSet<URI>();
+	private static Queue<URI> providers = new ArrayDeque<URI>();
+	
+	// Set of listeners to notify of changes
+	private static Set<ProviderStatusListener> listeners = new HashSet<ProviderStatusListener>();
 
 	// Demonstration function
 	public static void main(String[] args) throws InterruptedException {
@@ -41,7 +48,7 @@ public class JungleJeppsmDNS {
 		 JungleJeppsmDNS.startDiscovery();
 		 Thread.sleep(1000);
 		 
-		 for(URI provider : getProviders())
+		 for(URI provider : providers)
 			 System.out.println(provider);
 		 
 		 JungleJeppsmDNS.stopDiscovery();
@@ -50,12 +57,18 @@ public class JungleJeppsmDNS {
 		 JungleJeppsmDNS.stopBroadcasting();
 	 }
 	
-	public static URI[] getProviders(){
-		int count = providers.size();
-		URI[] providerArray = new URI[count];
-		return providers.toArray( providerArray );
+	/**
+	 * Get the oldest providers found that is still available. 
+	 * Returns null is there is no providers available.
+	 * @return
+	 */
+	public static URI getProvider(){
+		return providers.peek();
 	}
 	
+	/**
+	 * Starts searching for providers for Jungle Jepps
+	 */
 	public static void startDiscovery(){
 		 if(discovery == null)
 			try {
@@ -71,6 +84,9 @@ public class JungleJeppsmDNS {
 		 discovery.addServiceListener(PROPRIETARY_MDNS_SERVICE_TYPE, listener);
 	}
 	
+	/**
+	 * Stops searching a listening for changes for service providers
+	 */
 	public static void stopDiscovery(){
 		System.out.println("Stop listening for service changes...");
 		
@@ -133,6 +149,12 @@ public class JungleJeppsmDNS {
 		}
 	 }
 	 
+	 /**
+	  * This class implements the JmDNS Service listener and modifies
+	  * the providers queue and notifies any listeners of changes
+	  * @author joeljeske14
+	  *
+	  */
 	 static class Listener implements ServiceListener {
 			
 			/**
@@ -146,9 +168,21 @@ public class JungleJeppsmDNS {
 			 */
 			public void serviceRemoved(ServiceEvent serviceEvent) {
 				ServiceInfo info = serviceEvent.getInfo();
+				boolean primaryChange = false;
 				
 				for(String addr : info.getHostAddresses())
-					providers.remove( buildAddress( addr, info.getPort() ) );
+				{
+					
+					URI toRemove = buildAddress( addr, info.getPort() ) ;
+					
+					if( toRemove.equals( providers.peek() ) )
+						primaryChange = true;
+						
+					providers.remove( toRemove );
+				}
+				
+				if( primaryChange ) // Need to notify listeners
+					notifyListeners();	
 			}
 			
 			/**
@@ -157,9 +191,14 @@ public class JungleJeppsmDNS {
 			 */
 			public void serviceResolved(ServiceEvent serviceEvent) {
 				ServiceInfo info = serviceEvent.getInfo();
+				boolean notifyListeners = providers.isEmpty(); 
 				
 				for(String addr : info.getHostAddresses())
 					providers.add( buildAddress( addr, info.getPort() ) );
+				
+				// Need to notify listeners
+				if( notifyListeners  && !providers.isEmpty()) 
+					notifyListeners();				
 			}
 			
 			private URI buildAddress(String address, int port){
@@ -173,5 +212,49 @@ public class JungleJeppsmDNS {
 				
 				return uri; 
 			}
-		 }
+			
+			private void notifyListeners(){
+				URI newProvider = providers.peek();
+				for(ProviderStatusListener listener : listeners)
+					listener.newPrimaryProvider(newProvider);
+			}
+	}
+	 
+	
+	 /**
+	  * Register a listener to be notified of changes in providers
+	  * @param listener
+	  */
+	 public static void registerChangeListener(ProviderStatusListener listener){
+		 if(listener == null) return;
+		 
+		 listeners.add(listener);
+		 
+		 if( !providers.isEmpty() )
+			 listener.newPrimaryProvider( providers.peek() );
+	 }
+	 
+	 /**
+	  * Unregister a listener to stop receiving notifications of changes
+	  * @param listener
+	  */
+	 public static void unregisterChangeListener(ProviderStatusListener listener){
+		 listeners.remove(listener);
+	 }
+	 
+	 /**
+	  * This is a listener interface used to allow sub-clients to listen on
+	  * the statuses of providers.
+	  * @author joeljeske14
+	  *
+	  */
+	 public interface ProviderStatusListener {
+		 /**
+		  * This method will be called when the primary provider is changed. 
+		  * This is called when the primary provider is removed and another 
+		  * provider is available.
+		  * @param URI primary provider
+		  */
+		 void newPrimaryProvider(URI newProvider);
+	 }
 }
