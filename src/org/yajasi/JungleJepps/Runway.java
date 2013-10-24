71242301bb65500e4d0e04953d851031e48fafb2
joeljeske <joeljeske14@10.39.30.79>
2013-10-23 22:48:40 -0500

    Created base methods to access fields using ENUM

--- src/org/yajasi/JungleJepps/Runway.java
+++ src/org/yajasi/JungleJepps/Runway.java
@@ -1,30 +1,118 @@
 package org.yajasi.JungleJepps;
 
-import java.util.Map;
+import java.io.File;
+import java.sql.Date;
+
+import org.yajasi.JungleJepps.pdf.HtmlPreparer;
 
 public class Runway {
 	
-	// All the fields have string identifiers. Will be used in database
-	public static final String RUNWAY_ID = "id";
-	public static final String RUNWAY_NAME = "name";
-
-	private Map<String, Field> fields; 
+	private boolean isModified;
 	
-	public Runway(){
-		
+	public Runway(){	
 	}
 	
+	/**
+	 * Used to get the current value of Field for the loaded runway.
+	 * It will return the most recently edited value, not necessarily 
+	 * the value in the database. 
+	 * @param field
+	 * @return fieldValue
+	 */
+	public String getField(Field field){
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Used to get the label for a specific field. If the field does not have a 
+	 * label it returns an empty string.
+	 * @param field
+	 * @return label
+	 */
+	public String getLabel(Field field){
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Used to change the value of a field. Pass new value for the
+	 * for the field. Returns true if a change occurred and false if no
+	 * change occurred. 
+	 * @param field
+	 * @param value
+	 * @return modified
+	 */
+	public boolean setField(Field field, String value){
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Used to change the value of a field. Pass new value for the
+	 * for the field. Returns true if a change occurred and false if no
+	 * change occurred. 
+	 * @param field
+	 * @param value
+	 * @return modified
+	 */
+	public boolean setField(Field field, Boolean value){
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Used to change the value of a field. Pass new value for the
+	 * for the field. Returns true if a change occurred and false if no
+	 * change occurred. 
+	 * @param field
+	 * @param value
+	 * @return modified
+	 */
+	public boolean setField(Field field, Date value){
+		throw new UnsupportedOperationException();	
+	}
+	
+	/**
+	 * Used to determine if the settings indicate that this field
+	 * is to be pulled from an operations database and therefore is
+	 * a readonly field.
+	 * @param field
+	 * @return
+	 */
+	public boolean isFieldReadonly(Field field){
+		throw new UnsupportedOperationException();	
+	}
+	
+	/**
+	 * Used to indicate if this Runway object in current runtime reflects
+	 * what is stored in the database. 
+	 * @return
+	 */
 	public boolean isModified(){
-		for(Field f : fields.values())
-			if( f.isModified() )
-				return true;
-		
-		return false;
+		return isModified;
 	}
 	
-	public Field getField(String field_name){
-		return fields.get( field_name );
+	/**
+	 * Used to save the current state of the runway to the database
+	 */
+	public void save(){
+		throw new UnsupportedOperationException();		
 	}
 	
+	/**
+	 * Used to publish the Runway PDF to the repository.
+	 * @return publishedFile
+	 */
+	public File publish(){
+		return HtmlPreparer.publish(this);		
+	}
+
+	/**
+	 * Create a PDF preview of the Runway and store it temporarily.
+	 * @return publishedFile
+	 */
+	public File preview(){
+		File temp = new File("temp.pdf");
+		return HtmlPreparer.publish(this, temp);		
+	}
+	
+
 	
 }
