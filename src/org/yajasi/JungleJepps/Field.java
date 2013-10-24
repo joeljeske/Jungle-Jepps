1e1135f62631261946abcb60655a6120a9784c43
joeljeske <joeljeske14@10.39.30.79>
2013-10-23 20:57:02 -0500

    Field ENUM. USE THIS ENUM IN PLACE OF EVERY FIELD IDENTIFER. (e.g. Field.RUNWAY_IDENTIFIER.toString())

--- src/org/yajasi/JungleJepps/Field.java
+++ src/org/yajasi/JungleJepps/Field.java
@@ -1,51 +1,38 @@
 package org.yajasi.JungleJepps;
 
-public class Field {
-	
-	private Class<?> type;
-	private String name;
-	private Object value; 
-	private boolean isModified;
-	private boolean isReadOnly;
-	
-	public Field(String name, Class<?> type, Object value, boolean isReadOnly){
-		this.name = name;
-		this.type = type;
-		this.value = value;
-		this.isReadOnly = isReadOnly;
-		this.isModified = false;
-	}
-	
-	public boolean isType(Class<?> type){
-		return this.type.equals( type );
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public Object getValue(){
-		return value;
-	}
-	
-	public void setValue(Object value){
-		if( this.isReadOnly )
-			throw new UnsupportedOperationException("This field is readonly. Field: " + this.name);
-		
-		this.value = value;
-		this.isModified = true;
-	}
-	
-	public boolean isModified(){
-		return isModified;
-	}
-	
-	public void clearIsModified(){
-		this.isModified = false;
-	}
-	
-	public boolean isReadOnly(){
-		return isReadOnly;
-	}
+public enum Field {
+  RUNWAY_IDENTIFIER,
+  RUNWAY_NAME,
+  AIRCRAFT_IDENTIFIER,
+  LONGITUDE,
+  LATITUDE,
+  INSPECTION_NA,
+  INSPECTION_DATE,
+  INSPECTOR_NAME,
+  INSPECTION_DUE,
+  CLASSIFICATION,
+  FREQUENCY_1,
+  FREQUENCY_2,
+  LANGUAGE_GREET,
+  ELEVATION,
+  LENGTH,
+  WIDTH_TEXT,
+  TDZ_SLOPE,
+  IAS_ADJUSTMENT,
+  PRECIPITATION_ON_SCREEN,
+  RUNWAY_A,
+  A_TAKEOFF_RESTRICTION,
+  A_TAKEOFF_NOTE,
+  A_LANDING_RESTRICTION,
+  A_LANDING_NOTE,
+  RUNWAY_B,
+  B_TAKEOFF_RESTRICTION,
+  B_TAKEOFF_NOTE,
+  B_LANDING_RESTRICTION,
+  B_LANDING_NOTE;
+
+  public String toString(){
+	  return super.toString().toLowerCase();
+  }
 
 }
