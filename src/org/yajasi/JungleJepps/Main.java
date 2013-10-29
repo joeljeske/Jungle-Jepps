7f0d070dedd907986b4ee443cfa5a58b6f999826
joeljeske <joeljeske14@10.39.25.78>
2013-10-28 22:03:11 -0500

    Started building initial programatic tracer bullet

--- src/org/yajasi/JungleJepps/Main.java
+++ src/org/yajasi/JungleJepps/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		Runway run = new Runway();
+		//run.put(Field.RUNWAY_IDENTIFIER, 	"KIW");
+		//run.put(Field.RUNWAY_NAME, 			"Kiwi");
+		//run.put(field, value)
+		
 	}
 
 }
