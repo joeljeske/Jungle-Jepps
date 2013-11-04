28fad01ceac9cb1b1014dc237528156732c864ce
joeljeske <joeljeske14@10.39.25.78>
2013-11-04 10:24:24 -0600

    Added comments and function to save updated runway

--- src/org/yajasi/JungleJepps/Runway.java
+++ src/org/yajasi/JungleJepps/Runway.java
@@ -2,25 +2,38 @@ package org.yajasi.JungleJepps;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.yajasi.JungleJepps.db.DatabaseManager;
 import org.yajasi.JungleJepps.pdf.HtmlPreparer;
 
 public class Runway extends HashMap<Field, String> implements ValueByEnum {
 
-	private boolean isModified;
+	private Set<Field> modifiedFields;
 	
 	public Runway(){
 		super();
-		 isModified = false;
+		 modifiedFields = new HashSet<Field>();
 	}
 	
+	/**
+	 * Removes all Fields from the runway making it essentially a new Runway object
+	 */
 	public void removeAll(){
 		for(Field key : this.keySet())
 			this.remove(key);
+		modifiedFields.clear();
 	}
 	
+	/**
+	 * Get a Field from the Runway
+	 * @param key Field key
+	 * @return String value for field
+	 */
 	@Override
 	public String get(Object key){
 		Field field;
@@ -35,6 +48,12 @@ public class Runway extends HashMap<Field, String> implements ValueByEnum {
                 return val == null ? "" : val; //Return empty string instead of null
 	}
 	
+	/**
+	 * Set a new value for a field
+	 * @param field
+	 * @param value
+	 * @return
+	 */
 	@Override
 	public String put(Field field, String value){
 		if( isFieldReadonly(field) )
@@ -44,11 +63,14 @@ public class Runway extends HashMap<Field, String> implements ValueByEnum {
 		if(value != null && value.isEmpty())
 			value = null;
 		
-		value = super.put(field, value);
+		String oldValue = super.put(field, value);
 		
-		isModified = true;
+		// Old value cannot null unless new value is null and 
+		if( equals(oldValue, value) )
+			modifiedFields.add( field ); // This field is now modified
 		
-		return value;
+		
+		return oldValue;
 	}
 	
 	/**
@@ -59,7 +81,7 @@ public class Runway extends HashMap<Field, String> implements ValueByEnum {
 	 * @return
 	 */
 	public boolean isFieldReadonly(Field field){
-		return false;//DatabaseManager.getSettings().isFieldOverridden(field);
+		return DatabaseManager.getSettings().isFieldOverridden(field);
 	}
 	
 	/** 
@@ -68,15 +90,53 @@ public class Runway extends HashMap<Field, String> implements ValueByEnum {
 	 * @return
 	 */
 	public boolean isModified(){
-		return isModified;
+		// Must contain at least one entry to be modified
+		return !modifiedFields.isEmpty();
 	}
 	
 	/**
 	 * Used to save the current state of the runway to the database
 	 */
 	public void save(){
-		throw new UnsupportedOperationException();
-		// isModified = false;		
+		modifiedFields.add(Field.AIRCRAFT_IDENTIFIER);
+		modifiedFields.add(Field.RUNWAY_IDENTIFIER);
+		
+		Runway run = generateModifiedRunway();
+		
+		try {
+			DatabaseManager.getDatabase().updateRunway(run);
+			modifiedFields.clear();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		}		
+	}
+	
+	/**
+	 * Create a new runway object that only contains the fields and 
+	 * the values for Fields that are modified in the current runway object.
+	 * Used to save resources when updating items in the database. 
+	 * @return Runway object with only modified fields
+	 */
+	private Runway generateModifiedRunway(){
+		Runway run = new Runway();
+		run.putAll( getMapOfKeys(modifiedFields) );
+		return run;
+	}
+	
+	/**
+	 * Returns a new map of all the fields and values using the keys in 
+	 * this set 
+	 * @param keys set of fields
+	 * @return Map<Field, String> new map of keys from set
+	 */
+	private Map<Field, String> getMapOfKeys(Set<Field>  keys){
+		Map<Field, String> data = new HashMap<Field, String>();
+		for(Field f : keys)
+		{
+			data.put( f, this.get(f) );
+		}
+		return data;
 	}
 	
 	/**
@@ -97,6 +157,11 @@ public class Runway extends HashMap<Field, String> implements ValueByEnum {
 		return HtmlPreparer.publish(this, temp);		
 	}
 
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
 	@Override
 	public String get(Enum key) {
 		if(key instanceof Field)
@@ -105,6 +170,18 @@ public class Runway extends HashMap<Field, String> implements ValueByEnum {
 		return null; 
 	}
 	
+	/**
+	 * Are the two strings equal or both null
+	 * @param str
+	 * @param str2
+	 * @return true if they are equal
+	 */
+	private boolean equals(String str, String str2){
+		// The strings are the same reference or they are both not null and they
+		// contain identical characters.
+		return (str == str2) ||
+				(str != null && str2 != null && str.equals(str2));
+	}
 
 	
 }
