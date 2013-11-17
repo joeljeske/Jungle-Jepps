cddce2c74a0bcf711e3c32c83629cfc2672ea51b
Joel Jeske <joeljeske14@joel-jeske>
2013-11-17 00:12:11 -0600

    Supports listening for text areas, combo boxes, and checkboxes

--- src/org/yajasi/JungleJepps/ui/OnChange.java
+++ src/org/yajasi/JungleJepps/ui/OnChange.java
@@ -8,7 +8,9 @@ package org.yajasi.JungleJepps.ui;
 
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.text.JTextComponent;
 
 /**
  *
@@ -16,9 +18,36 @@ import javax.swing.JTextField;
  */
 public abstract class OnChange  {
     
-    private String old; 
+    private String old;
+    private int index;
     
-    public OnChange(final JTextField field){
+    public OnChange(final JComboBox menu){
+        // Listen for focus events to see if the value has changed
+        menu.addFocusListener(new FocusListener(){
+            @Override
+            public void focusGained(FocusEvent e) {
+            	//Save the original value of the field
+                index = menu.getSelectedIndex();
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+            	int newIndex = menu.getSelectedIndex();
+            	
+                if( index != newIndex ) //If the value has changed
+                {
+                    // Set value for future changes
+                    index = newIndex;
+                    
+                    // Call the change listener
+                    String text = menu.getItemAt(index).toString();
+                    onChange( text );
+                }
+            } 
+        });
+    }
+    
+    public OnChange(final JTextComponent field){
         // Listen for focus events to see if the value has changed
         field.addFocusListener(new FocusListener(){
             @Override
@@ -39,12 +68,37 @@ public abstract class OnChange  {
                     // Call the change listener
                     onChange( newVal );
                 }
-            }
-            
+            } 
         });
-        
-        
     }
+    
+     public OnChange(final JCheckBox field){
+        // Listen for focus events to see if the value has changed
+        field.addFocusListener(new FocusListener(){
+            @Override
+            public void focusGained(FocusEvent e) {
+            	//Save the original value of the field
+                index = field.isSelected() ? 1 : 0;
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                int newIndex = field.isSelected() ? 1 : 0;
+            	
+                if( index != newIndex ) //If the value has changed
+                {
+                    // Set value for future changes
+                    index = newIndex; 
+                    
+                    // Call the change listener
+                    String value = (index == 1 ? "true" : "false");
+                    onChange( value );
+                }
+            } 
+        });
+    }
+    
+    
     
     public abstract void onChange(String value);
 }
